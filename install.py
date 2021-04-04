bdeb5db426b99743c1352c9501715743d3361388
gjr80 <gjroderick@gmail.com>
2021-04-04 15:24:17 +1000

    changed config to triple quote string to better support comments

--- install.py
+++ install.py
@@ -21,7 +21,14 @@ Revision History
 """
 
 # python imports
+import configobj
 from distutils.version import StrictVersion
+try:
+    # Python 3
+    from io import StringIO
+except ImportError:
+    # Python 2
+    from StringIO import StringIO
 
 # WeeWX imports
 import weewx
@@ -29,7 +36,111 @@ import weewx
 from setup import ExtensionInstaller
 
 REQUIRED_VERSION = "4.0.0"
-WS_VERSION = "0.1.0"
+WS_VERSION = "0.1.0b3"
+
+ws_config = """
+[StdReport]
+    [[WEEWXtagsReport]]
+        skin = WEEWXtags
+        enable = True
+        [[[Units]]]
+            [[[[TimeFormats]]]]
+                date_f = %d/%m/%Y,
+                date_time_f = %d/%m/%Y %H:%M
+    [[ClientrawReport]]
+        skin = Clientraw
+        enable = True
+        [[[Units]]]
+            [[[[StringFormats]]]]
+                degree_C = %.1f
+                degree_compass = %.0f
+                foot = %.0f
+                hPa = %.1f
+                km = %.1f
+                knot = %.1f
+                mm = %.1f
+                percent = %.0f
+                uv_index = %.1f
+                watt_per_meter_squared = %.0f
+                NONE = --
+[DataBindings]
+    [[ws_binding]]
+        database = ws_sqlite
+        table_name = archive
+        manager = weewx.manager.DaySummaryManager
+        schema = user.wsschema.ws_schema
+    [[ws_supp_binding]]
+        database = ws_supp_sqlite
+        table_name = supp
+        manager = weewx.manager.Manager
+        schema = user.wsschema.ws_supp_schema
+[Databases]
+    [[ws_sqlite]]
+        database_type = SQLite
+        database_name = weewxwd.sdb
+    [[ws_supp_sqlite]]
+        database_type = SQLite
+        database_name = wdsupp.sdb
+    [[ws_mysql]]
+        database_type = MySQL
+        database_name = weewxwd
+    [[ws_supp_mysql]]
+        database_type = MySQL
+        database_name = wdsupp
+[WeewxSaratoga]
+    # WeewxSaratoga database binding
+    data_binding = ws_binding
+    
+    # radiation (solar insolation) level above which the sun is considered 
+    # shining
+    sunshine_threshold = 120
+    
+    [[Supplementary]]
+        # WeewxSaratoga supplementary database binding
+        data_binding = ws_supp_binding
+        [[[WU]]]
+            api_key = replace_me
+            enable = False
+        [[[DS]]]
+            api_key = replace_me
+            enable = False
+        [[[File]]]
+            file = /path/and/filename
+            enable = False
+            
+    [[RealtimeClientraw]]
+        # URL to use if transferring clientraw.txt to web server via HTTP POST 
+        # using post_clientraw.php. Supports both http and https. Must end with 
+        # post_clientraw.php. To enable uncomment line and enter web server 
+        # address to be used.
+        # remote_server_url = http://web.server.address/post_clientraw.php
+
+        # How often to generate clientraw.txt. clientraw.txt is only generated 
+        # on receipt of a loop packet and then only after at least min_interval 
+        # seconds have elapsed since the last generation time. Default is 10, 
+        # use 0 to generate on every loop packet.
+        min_interval = 10
+        
+        # Python date-time format strings. Format string codes as per 
+        # https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes
+         
+        # Date format. Recommended entries are:
+        #   date_format = %-m/%-d/%Y  # recommended for USA users
+        #   date_format = %-d/%-m/%Y  # recommended for non-USA users
+        date_format = %-d/%-m/%Y
+
+        # Long format times (HMS). Recommended entries are:
+        #   long_time_format = %-I:%M:%S_%p  # recommended for USA users
+        #   long_time_format = %H:%M:%S  # recommended for non-USA users 
+        long_time_format = %H:%M:%S
+        
+        # Short format times (HM). Recommended entries are:
+        #   short_time_format = %-I:%M_%p  # recommended for USA users
+        #   short_time_format = %H:%M  # recommended for non-USA users
+        short_time_format = %H:%M
+"""
+
+ws_dict = configobj.ConfigObj(StringIO(ws_config))
 
 
 def loader():
@@ -53,94 +164,7 @@ class WSInstaller(ExtensionInstaller):
             archive_services=['user.ws.WsArchive',
                               'user.ws.WsSuppArchive'],
             report_services=['user.rtcr.RealtimeClientraw'],
-            config={
-                'StdReport': {
-                    'WEEWXtagsReport': {
-                        'skin': 'WEEWXtags',
-                        'enable': 'True',
-                        'Units': {
-                            'TimeFormats': {
-                                'date_f': '%d/%m/%Y',
-                                'date_time_f': '%d/%m/%Y %H:%M'
-                            },
-                        },
-                    },
-                    'ClientrawReport': {
-                        'skin': 'Clientraw',
-                        'enable': 'True',
-                        'Units': {
-                            'StringFormats': {
-                                'degree_C': '%.1f',
-                                'degree_compass': '%.0f',
-                                'foot': '%.0f',
-                                'hPa': '%.1f',
-                                'km': '%.1f',
-                                'knot': '%.1f',
-                                'mm': '%.1f',
-                                'percent': '%.0f',
-                                'uv_index': '%.1f',
-                                'watt_per_meter_squared': '%.0f',
-                                'NONE': '--'
-                            },
-                        },
-                    }
-                },
-                'DataBindings': {
-                    'ws_binding': {
-                        'database': 'ws_sqlite',
-                        'table_name': 'archive',
-                        'manager': 'weewx.manager.DaySummaryManager',
-                        'schema': 'user.wsschema.ws_schema'
-                    },
-                    'ws_supp_binding': {
-                        'database': 'ws_supp_sqlite',
-                        'table_name': 'supp',
-                        'manager': 'weewx.manager.Manager',
-                        'schema': 'user.wsschema.ws_supp_schema'
-                    }
-                },
-                'Databases': {
-                    'ws_sqlite': {
-                        'database_type': 'SQLite',
-                        'database_name': 'weewxwd.sdb'
-                    },
-                    'ws_supp_sqlite': {
-                        'database_type': 'SQLite',
-                        'database_name': 'wdsupp.sdb'
-                    },
-                    'ws_mysql': {
-                        'database_type': 'MySQL',
-                        'database_name': 'weewxwd'
-                    },
-                    'ws_supp_mysql': {
-                        'database_type': 'MySQL',
-                        'database_name': 'wdsupp'
-                    }
-                },
-                'WeewxSaratoga': {
-                    'data_binding': 'ws_binding',
-                    'sunshine_threshold': '120',
-                    'Supplementary': {
-                        'data_binding': 'ws_supp_binding',
-                        'WU': {
-                            'api_key': 'replace_me',
-                            'enable': 'False'
-                        },
-                        'DS': {
-                            'api_key': 'replace_me',
-                            'enable': 'False'
-                        },
-                        'File': {
-                            'file': '/path/and/filename',
-                            'enable': 'False'
-                        }
-                    },
-                    'RealtimeClientraw': {
-                        'min_interval': '10',
-
-                    }
-                }
-            },
+            config=ws_dict,
             files=[('bin/user', ['bin/user/rtcr.py',
                                  'bin/user/stackedwindrose.py',
                                  'bin/user/wsastro.py',
