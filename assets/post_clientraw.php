eb9e7f32a7086ad272c1376e26d0c2d937dade09
ktrue <webmaster@saratoga-weather.org>
2021-04-11 07:34:45 -0700

    Version 0.1.1 - add HTTP response codes/text 200 - OK for proper operation 400 - Bad Request for malformed POST request (clientraw missing/malformed) 405 - Method Not Allowed for GET or HEAD requests 507 - Insufficient Storage if writing clientraw.txt to disk fails

--- assets/post_clientraw.php
+++ assets/post_clientraw.php
@@ -23,6 +23,13 @@ Version: 0.1.0                                        Date: 2 April 2021
 Revision History
   2 April 2021         v0.1.0
       - initial release
+  11 April 2021         v0.1.1   K. True mods
+      - added return codes for results
+        200 - OK for proper operation
+        400 - Bad Request for malformed POST request (clientraw missing/malformed)
+        405 - Method Not Allowed for GET or HEAD requests
+        507 - Insufficient Storage if writing clientraw.txt to disk fails
+
 
 Instructions for use:
 
@@ -33,35 +40,47 @@ Instructions for use:
     location of this file. The file name can be changed as well but should be
     left as clientraw.txt if using the received file with the Saratoga Weather
     Web Site templates or the Alternative dashboard.
-#TODO. Note about permissions/ownership.
 */
 
 // define our destination path and file name
 $cr_file = "./clientraw.txt";
 // we are only interested in HTTP POST
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    // get the data
-    $data = file_get_contents("php://input");
-    // the data should be urlencoded in key=value pairs, we want the pair whose
-    // key=='clientraw'
+  // get the data
+  $data = file_get_contents("php://input");
+  // the data should be urlencoded in key=value pairs, we want the pair whose
+  // key=='clientraw'
 
-    // iterate over each of the key=value pairs
-    foreach (explode('&', $data) as $chunk) {
-        // split the pair into key and value
-        $param = explode("=", $chunk);
-        // we are interested in the 'clientraw' data
-        if (urldecode($param[0]) == 'clientraw') {
-            $CRrec = urldecode($param[1]);
-            $CRarray = explode(' ',$CRrec);
-            // make sure it's a clientraw.txt record we've got
-            if(isset($CRarray[0]) and isset($CRarray[177]) and
-               $CRarray[0] == '12345' and preg_match('/^!!\S+!!/',$CRarray[177]) ) {
-               // save the decoded data to file
-               file_put_contents($cr_file, $CRrec);
-               // we have our data so exit the loop
-               break;
-            }
-        }
+  // iterate over each of the key=value pairs
+  foreach (explode('&', $data) as $chunk) {
+    // split the pair into key and value
+    $param = explode("=", $chunk);
+    // we are interested in the 'clientraw' data
+    if (urldecode($param[0]) == 'clientraw') {
+      $CRrec = urldecode($param[1]);
+      $CRarray = explode(' ',$CRrec);
+      // make sure it's a clientraw.txt record we've got
+      if(isset($CRarray[0]) and isset($CRarray[177]) and
+         $CRarray[0] == '12345' and preg_match('/^!!\S+!!/',$CRarray[177]) ) {
+       // save the decoded data to file
+       $flag = file_put_contents($cr_file, $CRrec);
+       if($flag !== false) {
+         // we have our data so exit the loop
+         header('HTTP/1.0 200 OK');
+         exit('<h1>200 OK</h1>');
+        } else {
+         header('HTTP/1.0 507 Insufficient Storage');
+         exit('<h1>507 Insufficient Storage</h1>');
+       }
+      }
     }
+  }
+} else { // GET or HEAD requests are rejected
+  header('HTTP/1.0 405 Method Not Allowed');
+  exit('<h1>405 Method Not Allowed</h1>');  
 }
-?>
+
+// oops.. malformed POST request.. let'em know
+header('HTTP/1.0 400 Bad Request');
+exit('<h1>400 Bad Request</h1>');  
+
